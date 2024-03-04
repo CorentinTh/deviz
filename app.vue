af60c52fe33a1b8f304179135e6eb73ceb2d50c1
Corentin Thomasset <corentin.thomasset74@gmail.com>
2024-03-05 00:40:09 +0100

    feat(seo): added meta tags

--- app.vue
+++ app.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+useHead({
+  title: 'Deviz - Developer Quiz Online',
+  meta: [
+    {
+      name: 'description',
+      content: 'Test your knowledge of HTTP status codes with this quiz.',
+    },
+    { name: 'author', content: 'Corentin Thomasset' },
+    {
+      name: 'keywords',
+      content: 'http, status, code, quiz, test, knowledge, developer, deviz',
+    },
+  ],
+});
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
