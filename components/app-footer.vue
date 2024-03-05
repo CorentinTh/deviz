cc9f4238d1c8d89724ddb8fbba96ba11cad1dd92
Corentin Thomasset <corentin.thomasset74@gmail.com>
2024-03-05 01:18:20 +0100

    feat(layout): added footer

---
+++ components/app-footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="border-t border-gray-800">
+    <UContainer class="text-gray-500 text-sm h-16 flex items-center justify-center">
+      <div>
+        <span>© {{ new Date().getFullYear() }} Deviz</span>
+        <span class="mx-2">•</span>
+        <span>Developed by </span>
+        <UButton label="Corentin Thomasset" color="gray-500" variant="link" to="https://corentin-thomasset.fr" target="_blank" :padded="false" />
+      </div>
+    </UContainer>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="w-full min-h-screen dark:bg-gray-900 text-sm relative font-sans">
+  <div class="w-full min-h-screen dark:bg-gray-900 text-sm relative font-sans flex flex-col">
     <app-header />
 
-    <UContainer>
+    <UContainer class="flex-1">
       <slot />
     </UContainer>
+
+    <app-footer />
   </div>
 </template>
